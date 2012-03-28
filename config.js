c0608e2d2fe45fdee4690ea4fe487f64993c8bb7
Info <info@zenforweb.com>
2012-03-27 23:58:29 -0600

    []

--- config.js
+++ config.js
@@ -7,6 +7,23 @@ exports.config = {
   'db_port':'3306',
   'db_pool_size':'20'
 }
+/*  NOTE:
+/* TO AVOID THE REDUNDANT config['config'] in the config['config']['app_port'] CALL (AND DB CONTROLLER INSTANTIATION), WE COULD DO:
+  exports.app_settings = {'
+    port':3737
+  }
+  exports.db_settings = {
+    'host':'localhost',
+    'user':'root',
+    etc..
+    etc..
+  }
+  
+  WOULD REQUIRE CHANGING HOW DB SETTINGS ARE REFERENCED IN DBASE CLASS INSTANTIATION
+    INSTEAD OF config['config']['db_user'], would become config['db_settings']['user']
+  WOULD REQUIRE CHANGING HOW PORT IS CALLED FOR APP.
+    INSTEAD OF config['config']['app_port'], would become config['app_settings']['port']
+*/
 
 /* ALIX
 exports.config = {
