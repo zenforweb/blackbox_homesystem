1ae212cbfb8aaaafd276c8a8d413a03cc206c869
Info <info@zenforweb.com>
2012-03-27 23:47:33 -0600

    integrating database controller

--- config.js
+++ config.js
@@ -1,4 +1,21 @@
+/*CHRIS*/
 exports.config = {
-  'app_port':3637,
-  'db':'test_db'
+  'app_port':3737,
+  'db_host':'localhost',
+  'db_user':'root',
+  'db_pass':'cleancut',
+  'db_port':'3306',
+  'db_pool_size':'20'
 }
+
+/* ALIX
+exports.config = {
+  'app_port':3737,
+  'db_host':'localhost',
+  'db_user':'root',
+  'db_pass':'cleancut',
+  'db_port':'3306',
+  'db_pool_size':'20'
+}
+*/
+
