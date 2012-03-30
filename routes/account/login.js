6592587194f4dd70326eb554fbbcf38e24e1286e
Info <info@zenforweb.com>
2012-03-29 21:04:45 -0600

    query example

--- routes/account/login.js
+++ routes/account/login.js
@@ -1,6 +1,13 @@
 exports.login = function(req, res){
-  res.render('index', {
-  	title: 	'Blackbox_user_test',
-  	test: 	'test'
+  var user = req.params['uid'];
+  var pass = req.params['pass'];
+  query_string = "SELECT * FROM logic_1.userSecure WHERE user='"+user+"' and pass='"+pass+"'";
+  res.app.settings['db'].query(query_string, function(result_object){
+    console.log(result_object);
+    /*add whatever to do after query here..*/
+    res.render('index', {
+      title:'Blackbox_user_test',
+      test:'test'
+    })
   })
 };
