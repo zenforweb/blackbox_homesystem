e97318d6ea3b2f676af28600ea5bb400566e6cfd
Info <info@zenforweb.com>
2012-03-31 22:33:01 -0600

    upgrade to using socket.io

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var db_base = require('./lib/dbase.js');
 var config = require('./config.js');
 var express = require('express');
 var routes = require('./routes');
+
 //var account_routes = require('./routes/account');
 var app = module.exports = express.createServer();
+var io = require('socket.io').listen(app);
 var db_controller = new db_base.Dbase_controller(config['config']['db_host'],config['config']['db_port'],config['config']['db_user'],config['config']['db_pass'],config['config']['db_pool_size']);
 db_controller.create_connection_pool();
 // Configuration
@@ -30,6 +32,17 @@ app.configure('production', function(){
 });
 // Routes
 app.get('/', routes.index);
-app.get('/account/login/:uid/:pass/', routes.account_login);
+io.sockets.on('connection', function(socket){
+  socket.on('login_attempt',function(login_data){
+    login_status=true;
+    console.log(login_data);
+    // QUERY based on login data
+    if (login_status==true){
+      socket.emit('login_success',{'status':true})
+    }else{
+      socket.emit('log_failure',{'status':false})
+    }
+  });
+})
 app.listen(config['config']['app_port']);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
