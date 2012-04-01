eb594caa4c450dc28a7bf8e21d8363ce86df95ab
Info <info@zenforweb.com>
2012-04-01 12:40:34 -0600

    deleted unused routes

--- routes/dashboard/entry.js
+++
@@ -1,8 +0,0 @@
-exports.dashboard = function(req, res){
-  query_string = "SELECT * FROM logic_1.userSecure WHERE user='"+user+"' and pass='"+pass+"'";
-  res.app.settings['db'].query(query_string, function(result_object){
-    console.log(result_object);
-    /*add whatever to do after query here..*/
-    res.send(result_object)
-  })
-};
