6011e22e27c2012f98fb93fe337b9fca63dec910
Alix Fullerton <alix@politeauthority.com>
2012-04-17 18:57:09 -0600

    fix for app crashing on refresh

--- app.js
+++ app.js
@@ -2,25 +2,23 @@
  * Module dependencies.
  */
 
-var db_base = require('./lib/dbase.js');
+
 var config = require('./config.js');
-var account = require('./lib/account.js');
-var dashboard = require('./lib/dashboard.js');
+
 
 var express = require('express');
 var routes = require('./routes');
-var jade = require('jade');
+
 var app = module.exports = express.createServer();
 var io = require('socket.io').listen(app);
 
-var db_controller = new db_base.Dbase_controller(config['db_settings']['db_host'],config['db_settings']['db_port'],config['db_settings']['db_user'],config['db_settings']['db_pass'],config['db_settings']['db_pool_size']);
-db_controller.create_connection_pool();
+
 
 // Configuration
 app.configure(function(){
   app.set('views', __dirname + '/public/');
   app.set('view engine', 'jade');
-  app.set('db', db_controller);
+  //app.set('db', db_controller);
   app.set('config', config);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -39,9 +37,24 @@ app.configure('production', function(){
 // Routes
 app.get('/', routes.index);
 io.sockets.on('connection', function(socket){
+  var db_base = require('./lib/dbase.js');
+  var account = require('./lib/account.js');
+  var dashboard = require('./lib/dashboard.js');
+  var jade = require('jade');
+  var db_controller = new db_base.Dbase_controller(config['db_settings']['db_host'],config['db_settings']['db_port'],config['db_settings']['db_user'],config['db_settings']['db_pass'],config['db_settings']['db_pool_size']);
+db_controller.create_connection_pool();
+  console.log('on connection')
   dashboard_controller = new dashboard.dashboard_controller(socket,jade,db_controller);
-  account = new account.account_controller(socket,jade,db_controller,dashboard_controller,config);
-})
+  console.log('post dashboard')
+  console.log(account)
+  console.log('post account')
+  try{
+    account = new account.account_controller(socket,jade,db_controller,dashboard_controller,config);
+  }
+  catch(err){
+    console.log( 'Error: ' + err );
+  }
+});
 
 app.listen(config['app_settings']['app_port']);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
