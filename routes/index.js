02306d74f5e741ca1cefa9baa671eab81e931da8
Alix Fullerton <alix@politeauthority.com>
2012-04-25 16:25:10 -0600

    created sys level views for install process, updated tranquility theme to pull library files such as jquery and jquery plugins that will be necisarry most additional themes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  */
 exports.index = function(req, res){
   /*app.settings['config']*/
+  /* './sys/views/index' 	for install theme */
   res.render('./themes/tranquility/views/index', {
     'title': 'Blackbox',
     'test': 'asas',
